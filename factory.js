5cd382edcb6c86f56d5dbadfa04796bf43257989
Dmitriy Bershadskiy <dmitriy.bershadskiy@dune-hd.com>
2015-02-01 16:33:45 +0200

    authurl

--- factory.js
+++ factory.js
@@ -1,6 +1,7 @@
 app.factory('crestService', function ($http, $location, $window) {
-    var rootUrl = "http://public-crest-sisi.testeveonline.com/";
+    var rootUrl = "https://api-sisi.testeveonline.com/";
     var oauthUrl = "https://sisilogin.testeveonline.com/oauth/authorize/?response_type=code&redirect_uri=" + encodeURIComponent('http://bershadskiy.github.io/#/auth') + "&client_id=bef0bb70a1d0430981755636671fcb93&scope=publicData&state=getcode";
+    var sOauthUrl = "https://sisilogin.testeveonline.com/oauth/token/?response_type=code&redirect_uri=" + encodeURIComponent('http://bershadskiy.github.io/#/auth') + "&client_id=bef0bb70a1d0430981755636671fcb93&scope=publicData&state=getcode";
 
     return {
         getData: function (tUrl) {
@@ -13,7 +14,7 @@ app.factory('crestService', function ($http, $location, $window) {
             });
         },
         redirectSSO: function () {
-            $window.location = oauthUrl;
+            $window.location = sOauthUrl;
         }
     }
 });
